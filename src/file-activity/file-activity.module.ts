ee98ad46088333dbe2f7ef7c8caf61bb2f6d75fc
Igihozo Belise <igihozobelise6@gmail.com>
2026-04-26 21:20:51 +0200

    updates

--- src/file-activity/file-activity.module.ts
+++ src/file-activity/file-activity.module.ts
@@ -7,5 +7,5 @@ import { DatabaseModule } from 'src/database/database.module';
   providers: [FileActivityService],
   exports: [FileActivityService],
 })
-
+// eslint-disable-next-line prettier/prettier
 export class FileActivityModule {}
